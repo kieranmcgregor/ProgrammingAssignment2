fb1d8cd67a85cc51ab37782a266df91ac91a9b25
Kieran McGregor <kieranmcgregor@gmail.com>
2015-06-12 00:37:56 -0400

    Added comments and code to cache the inverse of a square investable matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions will cache the inverse of a matrix if a cache
+## does not already exist
 
-## Write a short comment describing this function
+## This function will cache the inverse of a matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    solveMatrix <- NULL
+    set <- function(y) {
+        x <<- y
+        solveMatrix <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) solveMatrix <<- solve
+    getsolve <- function() solveMatrix
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## This function will check for a cache of the inverse if a matrix and
+## if none exists pass the matrix to makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    solveMatrix <- x$getsolve()
+    if(!is.null(solveMatrix)){
+        message("getting cached data")
+        return(solveMatrix)
+    }
+    data <- x$get()
+    solveMatrix <- solve(data)
+    x$setsolve(solveMatrix)
+    solveMatrix
 }
